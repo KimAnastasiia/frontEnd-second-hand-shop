fb0cb5583014bd42074c2fca7716f29d3ddd69a4
Anastasiia <anstasiakim2789@gmail.com>
2023-10-20 19:36:32 +0200

    created functions for delete and add favorites

--- src/Components/Products/Announcements.js
+++ src/Components/Products/Announcements.js
@@ -2,14 +2,14 @@ import { useEffect, useState, useRef } from "react";
 import { backendURL } from "../../Global";
 import { Card, Image, Modal, Button, Input, Radio, Space } from 'antd';
 import { Link } from "react-router-dom";
-import { useParams, useNavigate } from "react-router-dom";
+import Icon, { HomeOutlined } from '@ant-design/icons';
 import {
     allowSubmitForm,
     modifyStateProperty,
     joinAllServerErrorMessages, setServerErrors,
     validateFormDataInputRequired
 } from "../../Utils/UtilsValidations"
-import { async } from "q";
+import {HeartOutlined} from '@ant-design/icons';
 let Announcements = ({ openNotification }) => {
 
     let [products, setProducts] = useState([])
@@ -135,12 +135,68 @@ let Announcements = ({ openNotification }) => {
             openNotification("top", notificationMsg, "error")
         }
     }
+    let addToFavorites=async(productId)=>{
+  
+        let response = await fetch(backendURL + "/favorites/"+productId, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json ",
+                "apikey": localStorage.getItem("apiKey")
+            }
+        })
+
+        if (response.ok) {
+            openNotification("top", "Successfull", "success")
+        } else {
+            let responseBody = await response.json();
+            let serverErrors = responseBody.errors;
+            setServerErrors(serverErrors, setFormErrors)
+            let notificationMsg = joinAllServerErrorMessages(serverErrors)
+            openNotification("top", notificationMsg, "error")
+        }
+    }
+    const HeartSvg = () => (
+        <svg width="5em" height="2em" fill="currentColor" viewBox="0 0 1024 1024">
+          <path d="M923 283.6c-13.4-31.1-32.6-58.9-56.9-82.8-24.3-23.8-52.5-42.4-84-55.5-32.5-13.5-66.9-20.3-102.4-20.3-49.3 0-97.4 13.5-139.2 39-10 6.1-19.5 12.8-28.5 20.1-9-7.3-18.5-14-28.5-20.1-41.8-25.5-89.9-39-139.2-39-35.5 0-69.9 6.8-102.4 20.3-31.4 13-59.7 31.7-84 55.5-24.4 23.9-43.5 51.7-56.9 82.8-13.9 32.3-21 66.6-21 101.9 0 33.3 6.8 68 20.3 103.3 11.3 29.5 27.5 60.1 48.2 91 32.8 48.9 77.9 99.9 133.9 151.6 92.8 85.7 184.7 144.9 188.6 147.3l23.7 15.2c10.5 6.7 24 6.7 34.5 0l23.7-15.2c3.9-2.5 95.7-61.6 188.6-147.3 56-51.7 101.1-102.7 133.9-151.6 20.7-30.9 37-61.5 48.2-91 13.5-35.3 20.3-70 20.3-103.3 0.1-35.3-7-69.6-20.9-101.9z" />
+        </svg>
+    );
+    const HeartIcon = (props) => <Icon component={HeartSvg} {...props} />;
+
+    const deleteFavorites = async (productId) => {
+
+        let response = await fetch(backendURL + `/favorites/${productId}`, {
+            method: 'DELETE',
+            headers: {
+                "Content-Type" : "application/json ",
+                "apikey": localStorage.getItem("apiKey")
+            },
+        })
+        if (response.ok) {
+            openNotification("top", "Successfull", "success")
+        } else {
+            let responseBody = await response.json();
+            let serverErrors = responseBody.errors;
+            setServerErrors(serverErrors, setFormErrors)
+            let notificationMsg = joinAllServerErrorMessages(serverErrors)
+            openNotification("top", notificationMsg, "error")
+        }
+    };
     return (
         <Card title="All products">
 
             {products.map((product) =>
 
-                <Card style={{ marginTop: 16, marginLeft: 150, marginRight: 150 }} type="inner" title={product.title + " " + product.price + "€"} extra={localStorage.getItem("id") != product.sellerId && <Button type="primary" onClick={() => { showModal(product) }}>Buy</Button>}>
+                <Card style={{ marginTop: 16, marginLeft: 150, marginRight: 150 }} type="inner" title={product.title + " " + product.price + "€"} extra={[ <HeartIcon
+                    onClick={()=>{deleteFavorites(product.id)}}
+                    style={{
+                      color: 'hotpink'
+                    }}
+                  />,
+                  <HeartOutlined onClick={()=>{addToFavorites(product.id)}} style={{
+                    width:60,
+                    fontSize:25
+                  }}/>, 
+                  localStorage.getItem("id") != product.sellerId && <Button type="primary" onClick={() => { showModal(product) }}>Buy</Button>]}>
                     <Modal title="Choose cart" open={isModalOpen} onCancel={handleCancel} onOk={() => { handleOk() }}>
                         <Radio.Group onChange={onChange} value={selectedCreditCard} >
                             <Space direction="vertical">
