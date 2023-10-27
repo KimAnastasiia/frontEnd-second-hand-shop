51a0304316c4e1a90550d8ae41dcd9a312d56534
Anastasiia <anstasiakim2789@gmail.com>
2023-10-27 12:25:28 +0200

    created menu for filter announcements by price

--- src/Components/Products/Announcements.js
+++ src/Components/Products/Announcements.js
@@ -1,24 +1,30 @@
 import { useEffect, useState, useRef } from "react";
 import { backendURL } from "../../Global";
-import { Card, Image, Modal, Button, Radio, Space, Input } from 'antd';
-import { Link, Routes, Route, useNavigate, useLocation  } from "react-router-dom";
-import Icon, { HomeOutlined } from '@ant-design/icons';
+import { Card, Image, Modal, Button, Radio, Space, Input, InputNumber, Flex } from 'antd';
+import { Link, Routes, Route, useNavigate, useLocation } from "react-router-dom";
+import Icon, { AlignRightOutlined } from '@ant-design/icons';
+
 import {
     allowSubmitForm,
     modifyStateProperty,
     joinAllServerErrorMessages, setServerErrors,
     validateFormDataInputRequired
 } from "../../Utils/UtilsValidations"
-import {HeartOutlined} from '@ant-design/icons';
+import { HeartOutlined } from '@ant-design/icons';
 let Announcements = ({ openNotification }) => {
-    let navigate = useNavigate()
-    let [products, setProducts] = useState([])
+
+    const navigate = useNavigate()
+    const [products, setProducts] = useState([])
     const [formErrors, setFormErrors] = useState({})
     const [isModalOpen, setIsModalOpen] = useState(false);
     const [carts, setCarts] = useState([])
     const [selectedCreditCard, setSelectedCreditCard] = useState("");
-    let [search, setSearch] = useState("")
-    let currentProduct = useRef()
+    const [search, setSearch] = useState("")
+    const [minPrice, setMinPrice] = useState(0)
+    const [maxPrice, setMaxPrice] = useState(0)
+    const [openModel, setOpenModel]= useState(false)
+
+    const currentProduct = useRef()
     const { Search } = Input;
     useEffect(() => {
         getProducts();
@@ -77,6 +83,11 @@ let Announcements = ({ openNotification }) => {
         if (response.ok) {
             let jsonData = await response.json();
             setProducts(jsonData)
+            let temp = 0
+            jsonData.forEach((product)=>{
+                temp=Math.max(temp,product.price)
+            })
+            setMaxPrice(temp)
         } else {
             let responseBody = await response.json();
             let serverErrors = responseBody.errors;
@@ -96,10 +107,10 @@ let Announcements = ({ openNotification }) => {
                     "apikey": localStorage.getItem("apiKey")
                 },
                 body: JSON.stringify({
-                buyerId:localStorage.getItem("id"), 
-                id:id
-            })
-        });
+                    buyerId: localStorage.getItem("id"),
+                    id: id
+                })
+            });
 
         if (response.ok) {
             let jsonData = await response.json();
@@ -112,10 +123,10 @@ let Announcements = ({ openNotification }) => {
             openNotification("top", notificationMsg, "error")
         }
         getProducts();
-        addToTransactions()  
+        addToTransactions()
     }
     let addToTransactions = async () => {
-        currentProduct.current.buyerPaymentName=selectedCreditCard
+        currentProduct.current.buyerPaymentName = selectedCreditCard
         let response = await fetch(backendURL + "/transactions", {
             method: "POST",
             headers: {
@@ -137,9 +148,9 @@ let Announcements = ({ openNotification }) => {
             openNotification("top", notificationMsg, "error")
         }
     }
-    let addToFavorites=async(productId)=>{
-  
-        let response = await fetch(backendURL + "/favorites/"+productId, {
+    let addToFavorites = async (productId) => {
+
+        let response = await fetch(backendURL + "/favorites/" + productId, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json ",
@@ -159,7 +170,7 @@ let Announcements = ({ openNotification }) => {
     }
     const HeartSvg = () => (
         <svg width="5em" height="2em" fill="currentColor" viewBox="0 0 1024 1024">
-          <path d="M923 283.6c-13.4-31.1-32.6-58.9-56.9-82.8-24.3-23.8-52.5-42.4-84-55.5-32.5-13.5-66.9-20.3-102.4-20.3-49.3 0-97.4 13.5-139.2 39-10 6.1-19.5 12.8-28.5 20.1-9-7.3-18.5-14-28.5-20.1-41.8-25.5-89.9-39-139.2-39-35.5 0-69.9 6.8-102.4 20.3-31.4 13-59.7 31.7-84 55.5-24.4 23.9-43.5 51.7-56.9 82.8-13.9 32.3-21 66.6-21 101.9 0 33.3 6.8 68 20.3 103.3 11.3 29.5 27.5 60.1 48.2 91 32.8 48.9 77.9 99.9 133.9 151.6 92.8 85.7 184.7 144.9 188.6 147.3l23.7 15.2c10.5 6.7 24 6.7 34.5 0l23.7-15.2c3.9-2.5 95.7-61.6 188.6-147.3 56-51.7 101.1-102.7 133.9-151.6 20.7-30.9 37-61.5 48.2-91 13.5-35.3 20.3-70 20.3-103.3 0.1-35.3-7-69.6-20.9-101.9z" />
+            <path d="M923 283.6c-13.4-31.1-32.6-58.9-56.9-82.8-24.3-23.8-52.5-42.4-84-55.5-32.5-13.5-66.9-20.3-102.4-20.3-49.3 0-97.4 13.5-139.2 39-10 6.1-19.5 12.8-28.5 20.1-9-7.3-18.5-14-28.5-20.1-41.8-25.5-89.9-39-139.2-39-35.5 0-69.9 6.8-102.4 20.3-31.4 13-59.7 31.7-84 55.5-24.4 23.9-43.5 51.7-56.9 82.8-13.9 32.3-21 66.6-21 101.9 0 33.3 6.8 68 20.3 103.3 11.3 29.5 27.5 60.1 48.2 91 32.8 48.9 77.9 99.9 133.9 151.6 92.8 85.7 184.7 144.9 188.6 147.3l23.7 15.2c10.5 6.7 24 6.7 34.5 0l23.7-15.2c3.9-2.5 95.7-61.6 188.6-147.3 56-51.7 101.1-102.7 133.9-151.6 20.7-30.9 37-61.5 48.2-91 13.5-35.3 20.3-70 20.3-103.3 0.1-35.3-7-69.6-20.9-101.9z" />
         </svg>
     );
     const HeartIcon = (props) => <Icon component={HeartSvg} {...props} />;
@@ -169,7 +180,7 @@ let Announcements = ({ openNotification }) => {
         let response = await fetch(backendURL + `/favorites/${productId}`, {
             method: 'DELETE',
             headers: {
-                "Content-Type" : "application/json ",
+                "Content-Type": "application/json ",
                 "apikey": localStorage.getItem("apiKey")
             },
         })
@@ -183,41 +194,75 @@ let Announcements = ({ openNotification }) => {
             openNotification("top", notificationMsg, "error")
         }
     };
-    return (
-        <Card title="All products" extra={
-        
-            <Search
-                placeholder="input search text"
-                onChange={(e)=>{setSearch(e.target.value)}}
-                style={{
-                    width: 200,
-                }}
-            />
-        }>
+    const openModelDetails=()=>{
+        setOpenModel(true)
+    }
 
-            {products.filter((product)=>{
-                return product.title.toLowerCase().includes(search)
+    const onOkModelDetails=()=>{
+        setOpenModel(false)
+    }
+    const onCancelModelDetails=()=>{
+        setOpenModel(false)
+    }
+    return (
+        <Card title="All products"
+            extra={[
+                <div style={{ display: 'flex', alignItems: 'center' }}>
+                    <Search
+                        placeholder="input search text"
+                        onChange={(e) => { setSearch(e.target.value) }}
+                        style={{
+                            width: 200,
+                            marginRight: 20
+                        }}
+                    />
+                    <AlignRightOutlined
+                        style={{
+                            fontSize: "24px"
+                        }}
+                        onClick={openModelDetails}
+                    />
+                </div>
+            ]}
+        >
+            <Modal
+                title="Price"
+                open={openModel}
+                onOk={onOkModelDetails}
+                onCancel={onCancelModelDetails}
+            
+            > 
+                <Flex vertical>
+                    from: <InputNumber style={{marginBottom:20}} onChange={(e)=>{setMinPrice(e)}}></InputNumber>
+                    to: <InputNumber  onChange={(e)=>{setMaxPrice(e)}}></InputNumber>
+                </Flex>
+
+            </Modal>
+            {products.filter((product) => {
+                if(product.price>=minPrice && product.price<=maxPrice){
+                    return product.title.toLowerCase().includes(search) 
+                }
             }).map((product) =>
 
-                <Card style={{ marginTop: 16, marginLeft: 150, marginRight: 150 }} type="inner" title={product.title + " " + product.price + "€"} extra={[ 
-                <Button onClick={()=>{
-                    if(product.sellerId==localStorage.getItem("id")){
-                        navigate(`/profile/`+localStorage.getItem("email"))
-                    }else{
-                        navigate(`/seller/${product.sellerId}`)
-                    }
-                }}>{product.name}</Button>,
-                <HeartIcon
-                    onClick={()=>{deleteFavorites(product.id)}}
-                    style={{
-                      color: 'hotpink'
-                    }}
-                  />,
-                  <HeartOutlined onClick={()=>{addToFavorites(product.id)}} style={{
-                    width:60,
-                    fontSize:25
-                  }}/>, 
-                  localStorage.getItem("id") != product.sellerId && <Button type="primary" onClick={() => { showModal(product) }}>Buy</Button>]}>
+                <Card style={{ marginTop: 16, marginLeft: 150, marginRight: 150 }} type="inner" title={product.title + " " + product.price + "€"} extra={[
+                    <Button onClick={() => {
+                        if (product.sellerId == localStorage.getItem("id")) {
+                            navigate(`/profile/` + localStorage.getItem("email"))
+                        } else {
+                            navigate(`/seller/${product.sellerId}`)
+                        }
+                    }}>{product.name}</Button>,
+                    <HeartIcon
+                        onClick={() => { deleteFavorites(product.id) }}
+                        style={{
+                            color: 'hotpink'
+                        }}
+                    />,
+                    <HeartOutlined onClick={() => { addToFavorites(product.id) }} style={{
+                        width: 60,
+                        fontSize: 25
+                    }} />,
+                    localStorage.getItem("id") != product.sellerId && <Button type="primary" onClick={() => { showModal(product) }}>Buy</Button>]}>
                     <Modal title="Choose cart" open={isModalOpen} onCancel={handleCancel} onOk={() => { handleOk() }}>
                         <Radio.Group onChange={onChange} value={selectedCreditCard} >
                             <Space direction="vertical">
